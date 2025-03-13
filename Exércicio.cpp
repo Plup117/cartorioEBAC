e99ba8b69e6d7e90b6b5c69115c56590064223b8
Plup117 <lfc.lucasroberto@gmail.com>
2025-03-13 17:13:54 -0400

    Add files via upload

--- Exércicio.cpp
+++ Exércicio.cpp
@@ -123,7 +123,8 @@ int main()
 		printf("Escolha a opção desejada do menu\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n"); 
+		printf("\t3 - Deletar nomes\n\n");
+		printf("\t 4 - Encerrar o programa\n\n"); 
 		printf("Opção: ");//fim do menu
 				
 		scanf("%d", &opcao); 
@@ -142,6 +143,11 @@ int main()
 		
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Sistema finalizado. \n");
+			return 0;
 			break;
 		
 			default:
